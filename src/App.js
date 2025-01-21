4a8d2923c00658d7c076896afdc6b534c02d4060
dev_sk <sibu.kk@digitechsolutions.co.in>
2025-01-21 17:55:50 +0530

    Demo Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { StarknetProvider } from './context/StarknetProvider'
+import { ConnectWallet } from './components/ConnectWallet'
+import { TransferEth } from './components/TransferEth'
+
+
+function App() {
+  return (
+    <StarknetProvider>
+      <ConnectWallet />
+      <TransferEth />
+    </StarknetProvider>
+  );
+}
+
+export default App;
